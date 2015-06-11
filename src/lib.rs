93bd6e33142fd80c23c3d9b5bae0bb209b9c7325
March Liu <march.liu@gmail.com>
2015-06-11 23:52:35 +0800

    for the State<T> question.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(convert)]
 #![feature(collections)]
+#![feature(unboxed_closures)]
 
 pub mod functional;
 pub mod parsec;
--- src/parsec/combinator.rs
+++ src/parsec/combinator.rs
@@ -28,13 +28,42 @@ pub fn fail<T, S>(msg: String)->Parsec<(), S> where S:State<T> {
         Err(SimpleError::new(state.pos(), msg.clone()))
     })
 }
-//
-// struct Either<T, S>{
-//     x: Box<FnMut(S) -> Result<(), SimpleError>>>;
-//     y: Box<FnMut(S) -> Result<(), SimpleError>>>;
-// }
 
+pub struct Either<R, S>{
+    x: Parsec<R, S>,
+    y: Parsec<R, S>,
+}
 
+fn either<T, R, S>(x: Parsec<R, S>, y:Parsec<R, S>)->Either<R, S> where S:State<T>{
+    Either{
+        x: x,
+        y: y,
+    }
+}
+
+impl<R, S> FnOnce<(S, )> for Either<R, S> {
+    type Output = Parsec<R, S>;
+    extern "rust-call" fn call_once(self, args: (&mut S, )) -> Parsec<R, S> {
+        let (state, ) = args;
+        let pos = state.pos();
+        let val = self.x(state);
+        if val.is_ok() {
+            val
+        } else {
+            if pos == state.pos() {
+                self.y(state)
+            } else {
+                val
+            }
+        }
+    }
+}
+
+impl<R, S> FnMut<(S, )> for Either<R, S> {
+    extern "rust-call" fn call_mut(&mut self, args: (&mut S, )) -> Parsec<R, S> {
+        self.call_once(args)
+    }
+}
 
 // fn many<T, S>(parsec: Box<FnMut(&mut S)->Result<Arc<T>, SimpleError>>)
 //     -> Box<FnMut(&mut S)->Result<Vec<Arc<T>>, SimpleError>> {
