6581bd12ae70019b26d0682af9e1b1cc6f94acdb
March Liu <march.liu@gmail.com>
2015-06-15 19:05:57 +0800

    bind build passed

--- src/parsec/combinator.rs
+++ src/parsec/combinator.rs
@@ -84,14 +84,60 @@ impl<T:'static, R:'static> Either<T, R> {
         re
     }
 }
+// Type Continuation Pass
+pub struct Bind<T, C, P> {
+    parsec: Parsec<T, C>,
+    binder: Box<Fn(Arc<C>)->Parsec<T, P>>,
+}
 
+// type result passing
+// pub fn bind<T, R, P>(mut parsec:Parsec<T, R>, binder:Box<Fn(Arc<R>)->Parsec<T, P>>)->Parsec<T, P> {
+//     Box::new(move |state:&mut VecState<T>|-> Status<P>{
+//         parsec(state)
+//                 .map(|x:Arc<R>| binder(x.clone()))
+//                 .map(|mut p:Parsec<T, P>| p(state))
+//                 .unwrap_or_else(|err:SimpleError| Err(err))
+//     })
+// }
 
-// fn many<T, S>(parsec: Box<FnMut(&mut S)->Result<Arc<T>, SimpleError>>)
-//     -> Box<FnMut(&mut S)->Result<Vec<Arc<T>>, SimpleError>> {
+pub fn bind<T, C, P>(parsec:Parsec<T, C>, binder:Box<Fn(Arc<C>)->Parsec<T, P>>)->Bind<T, C, P> {
+    Bind{
+        parsec:parsec,
+        binder:binder,
+    }
+}
+
+impl<'a, T, C, P> FnOnce<(&'a mut VecState<T>, )> for Bind<T, C, P> {
+    type Output = Status<P>;
+    extern "rust-call" fn call_once(self, args: (&'a mut VecState<T>, )) -> Status<P> {
+        let (state, ) = args;
+        let mut s = self;
+        (s.parsec)(state)
+                .map(|x:Arc<C>| (s.binder)(x.clone()))
+                .map(|mut p:Parsec<T, P>| p(state))
+                .unwrap_or_else(|err:SimpleError| Err(err))
+    }
+}
+
+impl<'a, T, C, P> FnMut<(&'a mut VecState<T>, )> for Bind<T, C, P> {
+    extern "rust-call" fn call_mut(&mut self, args: (&'a mut VecState<T>, )) -> Status<P> {
+        self.call_once(args)
+    }
+}
+
+impl<T:'static, C:'static, P:'static> Bind<T, C, P>{
+    pub fn bind<Q>(self, binder:Box<Fn(Arc<P>)->Parsec<T, Q>>) -> Bind<T, P, Q> {
+        Bind{
+            parsec:Box::new(self),
+            binder:binder,
+        }
+    }
+}
+
+// fn many<T, R>(parsec: Parsec<T, R>) -> Parsec<T, Vec<R>> {
 //
 // }
 //
-// fn many1<T, S>(parsec: Box<FnMut(&mut S)->Result<Arc<T>, SimpleError>>)
-//     -> Box<FnMut(&mut S)->Result<Vec<Arc<T>>, SimpleError>> {
+// fn many1<T, S>(parsec: Parsec<T, R>) -> Parsec<T, Vec<R>> {
 //
 // }
