b6ddc7bf2ab4c0387f85fb64a504486e71117982
March Liu <march.liu@gmail.com>
2015-06-23 20:29:52 +0800

    some alpha and numeric parsers build passed

--- src/parsec/text.rs
+++ src/parsec/text.rs
@@ -39,6 +39,39 @@ pub fn digit() -> Bind<char, char> {
     }))
 }
 
+pub fn alpha() -> Bind<char, char> {
+    bind(bnd!(|state: &mut State<char>, x:char|->Status<char> {
+        if x.is_alphabetic() {
+            Ok(x)
+        } else {
+            let message = format!("Expect alphabetic but got {}", x);
+            Err(SimpleError::new(state.pos(), String::from(message)))
+        }
+    }))
+}
+
+pub fn alphanumeric() -> Bind<char, char> {
+    bind(bnd!(|state: &mut State<char>, x:char|->Status<char> {
+        if x.is_alphanumeric() {
+            Ok(x)
+        } else {
+            let message = format!("Expect alphabetic or number but got {}", x);
+            Err(SimpleError::new(state.pos(), String::from(message)))
+        }
+    }))
+}
+
+pub fn control() -> Bind<char, char> {
+    bind(bnd!(|state: &mut State<char>, x:char|->Status<char> {
+        if x.is_control() {
+            Ok(x)
+        } else {
+            let message = format!("Expect control but got {}", x);
+            Err(SimpleError::new(state.pos(), String::from(message)))
+        }
+    }))
+}
+
 pub fn uinteger() -> Monad<char, Vec<char>, String> {
     parser(arc!(many1(arc!(digit())))).bind(bnd!(|_:&mut State<char>, x:Vec<char>| -> Status<String> {
         Ok(x.iter().cloned().collect::<String>())
